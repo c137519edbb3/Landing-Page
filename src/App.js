aeb7143eb05d0686d24e3d74b84bcae44fa78056
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2024-10-05 01:05:02 +0500

    Add BookDemo section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Hero from './sections/Hero';
 import FAQ from './sections/FAQ';
+import BookDemo from './sections/BookDemo';
 
 function App() {
   return (
     <div>
       <Hero />
       <FAQ />
+      <BookDemo />
     </div>
   );
 }
---
+++ src/sections/BookDemo.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const BookDemo = () => {
+  return (
+    <section className="bg-white">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+        <h1 className="mb-4 text-2xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl">
+          Ready to scale your brand to{" "}
+          <span className="font-serif italic text-100">new heights?</span>
+        </h1>
+
+        <p className="text-lg font-normal text-gray-600 lg:text-lg sm:px-16 xl:px-48 max-w-5xl mx-auto">
+          If you want to achieve ground-breaking growth with increased sales and
+          profitability with paid ads, then you're in the right place.
+        </p>
+
+        {/* Blue Button */}
+        <button
+          type="button"
+          className="mt-6 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          Book your Demo
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default BookDemo;
